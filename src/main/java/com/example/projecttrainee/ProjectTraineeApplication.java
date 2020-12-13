c0c05e3c097044d163a643ed9f35c57fdca84087
VGsP <victorgabriel213@gmail.com>
2020-12-13 06:41:56 -0300

    let the training begins

---
+++ src/main/java/com/example/projecttrainee/ProjectTraineeApplication.java
@@ -0,0 +1,13 @@
+package com.example.projecttrainee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTraineeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTraineeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projecttrainee/ProjectTraineeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projecttrainee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTraineeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
